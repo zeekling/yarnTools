53a0762d257a76a4e81da9ce1156c8a32d2e1dfb
zeekling <lingzhaohui@zeekling.cn>
2025-11-26 23:59:25 +0800

    Support container summary

---
+++ src/main/java/org/hadoop/yarn/NMLevelDbOptions.java
@@ -0,0 +1,14 @@
+package org.hadoop.yarn;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class NMLevelDbOptions {
+
+    private String levelDbPath;
+
+    private boolean isContainer = false;
+}
--- src/main/java/org/hadoop/yarn/NodeManagerLevelDBTools.java
+++ src/main/java/org/hadoop/yarn/NodeManagerLevelDBTools.java
@@ -1,22 +1,74 @@
 package org.hadoop.yarn;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.cli.*;
+import org.apache.commons.lang3.StringUtils;
+import org.hadoop.yarn.state.RecoveredContainerState;
+import org.hadoop.yarn.state.RecoveredContainerStatus;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
  */
 public class NodeManagerLevelDBTools {
 
-    public static final Logger LOG = LoggerFactory.getLogger(NodeManagerLevelDBTools.class);
+    private static final Options cliOptions = initOptions();
 
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            throw new RuntimeException("Usage: NodeManagerLevelDBTools <levelDbPath>");
+    private static Options initOptions() {
+        Options cliOptions = new Options();
+        StateStoreOptions[] values = StateStoreOptions.values();
+        for (StateStoreOptions value : values) {
+            cliOptions.addOption(value.getOption());
         }
-        String levelDbPath = args[0];
-        LevelDBStateStore stateStore = new LevelDBStateStore(levelDbPath);
+        return cliOptions;
+    }
 
+    private static NMLevelDbOptions initNMLevelDbOptions(String[] args) throws ParseException {
+        CommandLineParser parser = new DefaultParser();
+        CommandLine commandLine = parser.parse(cliOptions, args);
+        NMLevelDbOptions.NMLevelDbOptionsBuilder builder = NMLevelDbOptions.builder();
+        builder.levelDbPath(commandLine.getOptionValue(StateStoreOptions.LEVELDB_PATH.getOption()));
+        builder.isContainer(commandLine.hasOption(StateStoreOptions.CONTAINER.getOption()));
+        return builder.build();
+    }
+
+    private static void printHelp() {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("NodeManagerLevelDBTools OPTIONS [leveldb_path...] \n\nOPTIONS", cliOptions);
+    }
+
+    private static void containerSummary(NMLevelDbOptions nmLevelDbOptions, LevelDBStateStore stateStore) {
+        List<RecoveredContainerState> allContainers = stateStore.getAllContainers();
+        Map<RecoveredContainerStatus, Integer> allContainersStatus = new HashMap<>();
+
+        for (RecoveredContainerState recoveredContainerState : allContainers) {
+            RecoveredContainerStatus status = recoveredContainerState.getStatus();
+            allContainersStatus.put(status, allContainersStatus.getOrDefault(status, 0) + 1);
+        }
+        System.out.println("=====================Containers Summary==============");
+        System.out.println("LevelDb path "+ nmLevelDbOptions.getLevelDbPath());
+        System.out.println("all containers " + allContainers.size());
+        for (Map.Entry<RecoveredContainerStatus, Integer> entry : allContainersStatus.entrySet()) {
+            System.out.println("container status " + entry.getKey() + " has " + entry.getValue() + " containers");
+        }
+        System.out.println("======================end============================");
+    }
+
+    public static void main(String[] args) throws ParseException {
+
+        NMLevelDbOptions nmLevelDbOptions = initNMLevelDbOptions(args);
+        if (StringUtils.isEmpty(nmLevelDbOptions.getLevelDbPath())) {
+            printHelp();
+            return;
+        }
+        String levelDbPath = nmLevelDbOptions.getLevelDbPath();
+        LevelDBStateStore stateStore = new LevelDBStateStore(levelDbPath);
+        if (nmLevelDbOptions.isContainer()) {
+            containerSummary(nmLevelDbOptions, stateStore);
+            return;
+        }
     }
 
 }
